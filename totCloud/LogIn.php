08bed7738e2e9d3a6022909f6255a98f66996ac6
Ti-trit <115530714+Ti-trit@users.noreply.github.com>
2024-11-26 11:50:42 +0100

    El LOG IN funciona

---
+++ totCloud/LogIn.php
@@ -0,0 +1,34 @@
+<?php
+if (!empty($_POST["submitButton"])) {
+    // Conexión a la base de datos
+    $conexion = new mysqli("localhost", "root", "", "PRACTICA2", "3306");
+    $conexion->set_charset("utf8");
+
+    // Obtener datos del formulario
+    $email = $_POST["emailField"];
+    $password = $_POST["passwordField"];
+    // Consulta SQL para obtener la contraseña más reciente
+    $stmt = $conexion->prepare("SELECT h.constrasenyaHash
+    FROM persona p
+    JOIN personal pl ON p.idPersona = pl.idPersonalCloud
+    JOIN HISTORIAL_CONTRASENYA h ON pl.idPersonalCloud = h.IdPersona
+    WHERE p.correuAcces = ?
+    ORDER BY h.dataCreacio DESC
+    LIMIT 1");
+    
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if ($datos = $result->fetch_object()) {
+        if (password_verify($password, $datos->constrasenyaHash)) {
+            echo "WELCOME HOME";
+        } else {
+            echo '<div class="alert alert-danger">ACCESS DENIED</div>';
+        }
+    } else {
+        echo '<div class="alert alert-danger">EMAIL NOT FOUND</div>';
+    }
+   
+}
+?>
---
+++ totCloud/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel = "stylesheet" type = "text/css" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <h1> WELCOME TO TOTCLOUD'S PORTAL </h1>
+    <form action="LogIn.php" method="POST">
+        <div class="dbl-field">
+            <div class="field">
+                <label for="">Email</label>
+                <input name ="emailField" type="text" placeholder="Enter your email" required>
+            </div>
+           
+            <div class=" "field>
+                <label for="">Password</label>
+               <input name ="passwordField" type="password" placeholder="Enter your password" required>     
+               
+            </div>
+            <input name ="submitButton" class ="btn" type ="submit" value ="LOG IN" >
+
+        </div>
+
+    </form>
+</div>
+   </header> 
+</body>    
+</html>
