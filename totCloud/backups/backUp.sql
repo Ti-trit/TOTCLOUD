def815bde62f27572ad4bd5ddd3b864df0b74265
Ti-trit <115530714+Ti-trit@users.noreply.github.com>
2024-12-10 00:42:10 +0100

    primer intento

---
+++ totCloud/backups/backUp.sql
@@ -0,0 +1,19 @@
+
+
+--CREATE TRIGGERS AFTER EVRY ACTION THAT CAN MODIFY THE DATABASE
+
+-- CREATE TRIGGER afeterInserts
+-- AFTER INSERT ON PERSONA 
+-- FOR EACH ROW
+-- BEGIN
+
+--     DECLARE @myVariable TEXT;
+--     SET @myVariable = CONCAT(
+--         'INSERT INTO PERSONA (nom, llinatges, correPersonal, dataNaixement, correuAccess, idPersona)',
+        
+--     )
+
+
+
+
+-- END;
