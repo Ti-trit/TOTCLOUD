5f6256b10eed6033bb0dcea4985f7922e3e3e90d
Ti-trit <115530714+Ti-trit@users.noreply.github.com>
2024-12-11 22:57:14 +0100

    updated event

--- totCloud/backups/backupIncremental.sql
+++ totCloud/backups/backupIncremental.sql
@@ -7,8 +7,8 @@ STARTS '2024-12-11 00:00:00'
 DO
 BEGIN
     -- Copiar registros nuevos desde la tabla de auditoría a la tabla de copias
-    INSERT INTO audit_table_user_copy (taula_modificada, fila_antiga, fila_nova, tipus_modificacio, data_modificacio, trx_timestamp, fecha_copia)
-    SELECT taula_modificada, fila_antiga, fila_nova, tipus_modificacio, data_modificacio, trx_timestamp, CURRENT_DATE
+    INSERT INTO audit_table_user_copy (taula_modificada, fila_antigua, fila_nova, tipus_modificacio, data_modificacio, trx_timestamp, fecha_copia)
+    SELECT taula_modificada, fila_antigua, fila_nova, tipus_modificacio, data_modificacio, trx_timestamp, CURRENT_DATE
     FROM audit_table_user AS audit
     WHERE NOT EXISTS (
         SELECT 1 
@@ -19,3 +19,21 @@ BEGIN
     );
 
 END; 
+
+
+DELIMITER $$
+
+CREATE EVENT backup_incremental_user_audit
+ON SCHEDULE EVERY 1 DAY  -- El evento se ejecutará cada 5 minutos
+STARTS CURRENT_TIMESTAMP  -- Inicia el evento inmediatamente
+DO
+BEGIN
+    -- Borra los registros anteriores en la tabla de backup (audit_table_user_copy)
+    DELETE FROM audit_table_user_copy;
+
+    -- Inserta los registros cambiados en los últimos 5 minutos desde la tabla de auditoría original (audit_table_user)
+    INSERT INTO audit_table_user_copy (taula_modificada, fila_antigua, fila_nova, tipus_modificacio, data_modificacio, trx_timestamp, fecha_copia)
+    SELECT taula_modificada, fila_antigua, fila_nova, tipus_modificacio, data_modificacio, trx_timestamp, CURRENT_TIMESTAMP
+    FROM audit_table_user
+    WHERE data_modificacio >= NOW() - INTERVAL 1 DAY;  -- Filtra por los últimos 5 minutos 
+END ;
