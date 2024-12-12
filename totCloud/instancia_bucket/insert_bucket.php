57833e177987039de9e7e6f6edaa5fba5baccb93
Ti-trit <115530714+Ti-trit@users.noreply.github.com>
2024-12-12 22:00:00 +0100

    Update insert_bucket.php

--- totCloud/instancia_bucket/insert_bucket.php
+++ totCloud/instancia_bucket/insert_bucket.php
@@ -6,12 +6,13 @@ include "../atributsClasses/instancia_bucket.php";
 $db = new Database($conn);
 $file = "llista_bucket.php";
 
+echo $_POST[$a1];
 // Verificar si los parámetros GET están definidos y no están vacíos
-if (!empty($_GET[$a1]) && !empty($_GET[$a2]) && !empty($_GET[$a3])) {
+if (!empty($_POST[$a1]) && !empty($_POST[$a2]) && !empty($_POST[$a3])) {
     // Recoger valores GET
-    $b1 = $_GET[$a1];
-    $b2 = $_GET[$a2];
-    $b3 = $_GET[$a3];
+    $b1 = $_POST[$a1];
+    $b2 = $_POST[$a2];
+    $b3 = $_POST[$a3];
 
     // Crear la consulta SQL
     $re1 = "INSERT INTO instancia_bucket ($a1, $a2, $a3, $a4) 
