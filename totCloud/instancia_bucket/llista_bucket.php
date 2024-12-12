cb20d9ac7d3133e18f772ac126532d759f9a06d1
Ti-trit <115530714+Ti-trit@users.noreply.github.com>
2024-12-12 22:28:25 +0100

    Add files via upload

--- totCloud/instancia_bucket/llista_bucket.php
+++ totCloud/instancia_bucket/llista_bucket.php
@@ -46,10 +46,10 @@ include "../header.php";
 
         // Generar URLs de modificación y eliminación
         $k = $reg[$pk];
-        $modifyUrl = "<a class='modify-link' href='/php/TOTCLOUD/totcloud/instancia_bucket/modifica_bucket.php?$pk=$k'>
+        $modifyUrl = "<a class='modify-link' href='modifica_bucket.php?$pk=$k'>
                     <span class='modify'>&#9999;</span>
                     </a>";
-        $deleteUrl = "<a class='delete-link' href='/php/TOTCLOUD/totcloud/instancia_bucket/delete_bucket.php?$pk=$k'>
+        $deleteUrl = "<a class='delete-link' href='delete_bucket.php?$pk=$k'>
                     <span class='delete'>&#10060;</span>
                     </a>";
         echo "<TD>$modifyUrl</TD>";
